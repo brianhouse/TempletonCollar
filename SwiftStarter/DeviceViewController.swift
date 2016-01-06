50757b52f578915429b4efddcc2ac3b54d7f9665
brianhouse <brian.house@gmail.com>
2016-01-06 11:13:42 -0500

    working battery and rssi buttons

--- SwiftStarter/DeviceViewController.swift
+++ SwiftStarter/DeviceViewController.swift
@@ -12,6 +12,14 @@ class DeviceViewController: UITableViewController {
     
     @IBOutlet weak var connectionState: UILabel!
     @IBOutlet weak var deviceName: UILabel!
+    @IBOutlet weak var deviceID: UILabel!
+    @IBOutlet weak var mfgNameLabel: UILabel!
+    @IBOutlet weak var serialNumLabel: UILabel!
+    @IBOutlet weak var hwRevLabel: UILabel!
+    @IBOutlet weak var fwRevLabel: UILabel!
+    @IBOutlet weak var modelNumberLabel: UILabel!
+    @IBOutlet weak var batteryLevelLabel: UILabel!
+    @IBOutlet weak var rssiLevelLabel: UILabel!
     
     var device: MBLMetaWear!
     
@@ -20,7 +28,7 @@ class DeviceViewController: UITableViewController {
         
         device.addObserver(self, forKeyPath: "state", options: NSKeyValueObservingOptions.New, context: nil)
         device.connectWithHandler { (error: NSError?) -> Void in
-            NSLog("We are connected")
+            self.deviceConnected()
         }
     }
 
@@ -33,18 +41,56 @@ class DeviceViewController: UITableViewController {
     
     
     override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
-        deviceName.text = device.name;
+        self.deviceName.text = device.name;
         switch (device.state) {
-        case .Connected:
-            connectionState.text = "Connected";
-        case .Connecting:
-            connectionState.text = "Connecting";
-        case .Disconnected:
-            connectionState.text = "Disconnected";
-        case .Disconnecting:
-            connectionState.text = "Disconnecting";
-        case .Discovery:
-            connectionState.text = "Discovery";
+            case .Connected:
+                connectionState.text = "Connected"; // dont update this here, I guess
+            case .Connecting:
+                connectionState.text = "Connecting";
+            case .Disconnected:
+                connectionState.text = "Disconnected";
+            case .Disconnecting:
+                connectionState.text = "Disconnecting";
+            case .Discovery:
+                connectionState.text = "Discovery";
         }
     }
+    
+    func deviceConnected() {
+        NSLog("deviceConnected");
+        
+        NSLog("ID: " + self.device.identifier.UUIDString);
+        self.deviceID.text = self.device.identifier.UUIDString;
+        
+        if let deviceInfo = self.device.deviceInfo {
+            self.mfgNameLabel.text = deviceInfo.manufacturerName;
+            self.serialNumLabel.text = deviceInfo.serialNumber;
+            self.hwRevLabel.text = deviceInfo.hardwareRevision;
+            self.fwRevLabel.text = deviceInfo.firmwareRevision;
+            self.modelNumberLabel.text = deviceInfo.modelNumber;
+        }
+        self.readBatteryPressed();
+        self.readRSSIPressed();
+        
+    }
+    
+    @IBAction func readBatteryPressed(sender: AnyObject?=nil) {
+        NSLog("readBatteryPressed");
+        self.device.readBatteryLifeWithHandler({ (number: NSNumber?, error: NSError?) -> Void in
+            if let n = number {
+                self.batteryLevelLabel.text = n.stringValue + "%";
+            }
+        });
+    }
+    
+
+    @IBAction func readRSSIPressed(sender: AnyObject?=nil) {
+        NSLog("readRSSIPressed");
+        self.device.readRSSIWithHandler({ (number: NSNumber?, error: NSError?) -> Void in
+            if let n = number {
+                self.rssiLevelLabel.text = n.stringValue + "";
+            }
+        });
+    }
+    
 }
